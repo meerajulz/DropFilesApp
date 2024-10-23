9c9a3d932e3479b9620f876cfb9dccf0817e1137
Juliana Leon <juleon80@gmail.com>
2024-10-23 23:02:03 +0200

    feat: implement file upload functionality

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,13 +1,22 @@
-<script>
+<script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Navigation from '$lib/components/Nav/Navigation.svelte';
 	import Search from '$lib/components/Search.svelte';
-	let userInitial = 'MJ';
-	let user = 'MeeraJulz';
+	let userInitial = 'MJ'; // to be replaced with user's initials
+	// to store updated files
+	let files: any[] = [];
 
-	function handleAddFiles() {
-		console.log('Add files');
+	//handle file input change
+	function handleFileChange(event: Event) {
+		const input = event.target as HTMLInputElement;
+		const selectedFiles = Array.from(input.files || []);
+		files = [...files, ...selectedFiles];
+		console.log(files);
+	}
+	//trigger file input from button click
+	function triggerFileInput() {
+		document.getElementById('file-input')?.click();
 	}
 </script>
 
@@ -24,8 +33,34 @@
 
 			<!-- Button to add files -->
 			<div class="mt-4">
-				<Button label="+ Add files" fullWidth={false} onClick={handleAddFiles} />
+				<Button label="+ Add files" fullWidth={false} onClick={triggerFileInput} />
+			</div>
+			<!-- File input -->
+			<input
+				type="file"
+				id="file-input"
+				accept=".csv,.xlsx,.json,.xls, .pdf, .doc, .docx"
+				multiple
+				class="hidden"
+				on:change={handleFileChange}
+			/>
+
+			<!-- Display the list of uploaded files -->
+			<div class="mt-6">
+				{#if files.length > 0}
+					<ul>
+						{#each files as file}
+							<li class="mb-2 text-gray-700">{file.name}</li>
+						{/each}
+					</ul>
+				{/if}
+				{#if files.length === 0}
+					<p class="text-gray-500">No files uploaded yet.</p>
+				{/if}
 			</div>
 		</section>
 	</div>
 </main>
+
+<style>
+</style>
